029d6f78ad016ed75e96a06224f86fd391520efc
Sravani Mandava <srisravani225@gmail.com>
2019-04-12 23:48:18 -0500

    Working api's with query parameters

--- weather/__init__.py
+++ weather/__init__.py
@@ -6,17 +6,15 @@ from flask_restful import Api
 import weather.database_model
 from weather.api.token import Token_transactions,token_cheker,token_blueprint
 from weather.api.weather import weather_blueprint,weather_data
-from weather.api.weather_zipcode import weather_zip_blueprint,weather_zipcode
 
-api = Api(token_blueprint,weather_blueprint,weather_zip_blueprint)
+
+api = Api(token_blueprint,weather_blueprint)
 app.register_blueprint(token_blueprint)
 app.register_blueprint(weather_blueprint)
-app.register_blueprint(weather_zip_blueprint)
 
 api.add_resource(Token_transactions,'/token')
 api.add_resource(token_cheker,'/token/<token_id>')
-api.add_resource(weather_data,'/weather/<city_name>')
-api.add_resource(weather_zipcode,'/weather_zip/<zip_code>')
+api.add_resource(weather_data,'/weather')
 
 
 if __name__=='__main__':
--- weather/api/weather.py
+++ weather/api/weather.py
@@ -1,5 +1,5 @@
 import json
-from flask import Response,Blueprint
+from flask import Response,Blueprint,request
 from weather.middleware.token_verification import is_expired
 from flask_restful import Resource
 import requests
@@ -12,19 +12,41 @@ weather_blueprint = Blueprint('weather_blueprint',__name__)
 class weather_data(Resource):
 
     @is_expired
-    def get(self,city_name):
-        return Response(json.dumps(weather_data.get_weather(city_name)),status=200,mimetype=JSON_MIME_TYPE)
+    def get(self):
+        args = request.args
+        return Response(json.dumps(weather_data.get_weather(args['type'],args['value'])),status=200,mimetype=JSON_MIME_TYPE)
 
     @lru_cache(maxsize=10)
-    def get_weather(city_name):
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-        data =[]
-        r = requests.get(url.format(city_name)).json()
-        weather ={
-            'city':city_name,
-            'temperature':r['main']['temp'],
-            'description':r['weather'][0]['description'],
-            'icon':r['weather'][0]['icon'],
-        }
-        data.append(weather)
-        return data
+    def get_weather(type,value):
+        if type=='name':
+            url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+            data =[]
+            r = requests.get(url.format(value)).json()
+            weather ={
+                'city':value,
+                'temperature':r['main']['temp'],
+                'description':r['weather'][0]['description'],
+                'icon':r['weather'][0]['icon'],
+            }
+            data.append(weather)
+            return data
+        elif type=='zip':
+            data = []
+            url1 = 'http://api.zippopotam.us/us/{}'
+            r1 = requests.get(url1.format(value)).json()
+            city_name = {
+                'city_name': r1['places'][0]['place name']
+            }
+            url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+            data = []
+            r = requests.get(url.format(city_name['city_name'])).json()
+            weather = {
+                'city': city_name['city_name'],
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'],
+            }
+            data.append(weather)
+            return data
+
+
